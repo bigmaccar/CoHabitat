8bc406262eda3ce22bd7989b920f180818f51e56
Jason Scheffel <contact@jasonscheffel.com>
2026-04-27 10:27:58 -0400

    feat(search): add saved apartment listings
    Signed-off-by: Jason Scheffel <contact@jasonscheffel.com>

--- client/src/Search.js
+++ client/src/Search.js
@@ -1,59 +1,87 @@
-import { BrowserRouter, Link, Routes, Route, Navigate } from "react-router-dom";
-import React, {useState, useEffect} from "react";
-import axios from 'axios';
+import { Link } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+
+const apartments = [
+    {
+        id: "apartment-1",
+        name: "Apartment 1",
+        image: require("./images/apartment1.jpg"),
+        description: "Two bedroom apartment near campus.",
+        rent: "$1200/month",
+        location: "Albany, NY"
+    },
+    {
+        id: "apartment-2",
+        name: "Apartment 2",
+        image: require("./images/apartment2.jpg"),
+        description: "Quiet apartment with shared living space.",
+        rent: "$950/month",
+        location: "Troy, NY"
+    }
+];
 
 function Search(){
+    const [savedListings, setSavedListings] = useState([]);
+
+    useEffect(() => {
+        const storedListings = JSON.parse(localStorage.getItem("savedListings") || "[]");
+        setSavedListings(storedListings);
+    }, []);
+
+    function toggleSavedListing(apartmentId) {
+        const updatedListings = savedListings.includes(apartmentId)
+            ? savedListings.filter(id => id !== apartmentId)
+            : [...savedListings, apartmentId];
+
+        setSavedListings(updatedListings);
+        localStorage.setItem("savedListings", JSON.stringify(updatedListings));
+    }
+
+    const savedApartmentDetails = apartments.filter(apartment => savedListings.includes(apartment.id));
 
     return (
-        <>
-        <section class="layoutSearch">
-            <div class="sidebarSearch">
-                <div class = "search">
+        <section className="layoutSearch">
+            <div className="sidebarSearch">
+                <div className="search">
                     <h1>Apartment Search</h1>
-                    <Link to = "/Apartment">Temp apartment page - make into full page, delete this link later</Link>
+                    <Link to="/Apartment">View apartment details</Link>
                 </div>
-                <input type="text" placeholder="Search" class = "search"></input>
-                <br></br>
-                <div class = "apartmentListWrapper"> {/* The wrapper is the box itself, contains all of the information. Duplicate this and the information in it per apartment*/}
-                    <div class = "apartmentListImage">
-                        <img src={require('.//images/apartment1.jpg')}/>
+                <input type="text" placeholder="Search" className="search" />
+                <br />
+                {apartments.map(apartment => (
+                    <div className="apartmentListWrapper" key={apartment.id}>
+                        <div className="apartmentListImage">
+                            <img src={apartment.image} alt={apartment.name} />
+                        </div>
+                        <div className="apartmentList">
+                            <h1>{apartment.name}</h1>
+                            <p>{apartment.description}</p>
+                            <p>{apartment.location}</p>
+                            <p>{apartment.rent}</p>
+                            <button onClick={() => toggleSavedListing(apartment.id)}>
+                                {savedListings.includes(apartment.id) ? "Unsave" : "Save"}
+                            </button>
+                        </div>
                     </div>
-                    <div class = "apartmentList"> {/* This is the actual apartment information, contained in the box*/}
-                        <h1>Apartment 1</h1> {/*Link container to Apartment */}
-                        <p>desc</p>
-                        <p>more information</p>
-                    </div>
-                </div>
-                <div class = "apartmentListWrapper">
-                    <div class = "apartmentListImage">
-                        <img src={require('.//images/apartment2.jpg')}/>
-                    </div>
-                    <div class = "apartmentList"> {/* This is the actual apartment information, contained in the box*/}
-                        <h1>Apartment 2</h1> {/* Link container to Apartment */}
-                        <p>desc</p>
-                        <p>more information</p>
-                    </div>
+                ))}
+            </div>
+            <div className="bodySearch">
+                <div className="filters">
+                    <h2>Saved Listings</h2>
+                    {savedApartmentDetails.length === 0 ? (
+                        <p>No saved listings yet.</p>
+                    ) : (
+                        savedApartmentDetails.map(apartment => (
+                            <div key={apartment.id}>
+                                <h3>{apartment.name}</h3>
+                                <p>{apartment.location}</p>
+                                <p>{apartment.rent}</p>
+                            </div>
+                        ))
+                    )}
                 </div>
             </div>
-            <div class="bodySearch">
-                <div class = "filters"> {/* Update later with decided filters. Currently includes different option types*/}
-                    <p>OPTION 1</p>
-                    <label class="switch" for="option1">
-                        <input type="checkbox" id="option1" value = "option1"/>
-                        <span class="slider round"></span>
-                    </label>
-                    <p>OPTION 2</p>
-                    <input type="checkbox" id="size1" name="size1" value="small"/>
-                    <label for="option2"> Small</label><br></br>
-                    <input type="checkbox" id="size2" name="size2" value="large"/>
-                    <label for="size2"> Large</label><br></br>
-                    <p>OPTION 3</p>
-                    <div class="slidecontainer">
-                    <input type="range" min="1" max="100" value="50" class="sliderBar" id="myRange"/>
-                    </div>
-                </div>
-            </div>
-        </section></>
+        </section>
     )
 
 }
